cafb5799edc9c84771040b4a6e5641c53283cf21
Success Obayuwana <succcessful.obayuwana@charisol.io>
2025-12-18 13:05:03 +0100

    add header, navbar and hero for mobile view

---
+++ components/Hero.tsx
@@ -0,0 +1,52 @@
+import Link from "next/link";
+import React from "react";
+
+const links = [
+  {
+    label: "documentary & tactual TV",
+    href: "/",
+  },
+  {
+    label: "narrative & fiction",
+    href: "/",
+  },
+  {
+    label: "digital content & corporate",
+    href: "/",
+  },
+];
+
+const Hero = () => {
+  return (
+    <section className="text-white">
+      <div className="flex flex-col justify-center items-center h-screen">
+        <p className="text-xl">
+          As a filmmaker and editor, I am passionate about bringing stories to
+          life, crafting impactful films that resonate and inspire.
+        </p>
+        <div className="flex flex-col items-center gap-5 mt-10">
+          {links.map((link, index) => (
+            <Link
+              href={link.href}
+              key={index}
+              // className="border-2 border-white rounded-full p-5 w-52"
+              className="w-52 rounded-full p-5 bg-white/10
+  backdrop-blur-xl border border-white/30 shadow-lg transition-all duration-300
+  hover:bg-white/20 hover:scale-105 active:scale-95 relative overflow-hidden
+before:absolute before:inset-0
+before:bg-linear-to-br before:from-white/20 before:to-transparent
+before:opacity-0 hover:before:opacity-100
+before:transition-opacity"
+            >
+              <p className="capitalize text-xl text-center font">
+                {link.label}
+              </p>
+            </Link>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
---
+++ components/MobileNavbar.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import Link from "next/link";
+import React, { useState } from "react";
+
+const navLinks = [
+  {
+    label: "about",
+    href: "/",
+  },
+  {
+    label: "gear",
+    href: "/",
+  },
+  {
+    label: "get in touch",
+    href: "/",
+  },
+];
+
+const MobileNavbar = () => {
+  const [openMenu, setOpenMenu] = useState(false);
+  return (
+    <>
+      <nav className="relative z-50 flex justify-between items-center px-5 pt-7 text-white">
+        <h1
+          className={`text-lg
+          ${openMenu ? " text-black" : "top-2 text-white"}
+        `}
+        >
+          Courage Obayuwana Films
+        </h1>
+
+        <button
+          onClick={() => setOpenMenu((prev) => !prev)}
+          aria-label="Toggle menu"
+          className="relative w-8 h-8 flex items-center justify-center cursor-pointer"
+        >
+          <span
+            className={`absolute h-0.5 w-8 transition-all duration-300 ease-in-out
+          ${openMenu ? "rotate-45 top-1/2 bg-black" : "top-2 bg-white"}
+        `}
+          />
+          <span
+            className={`absolute h-0.5 w-8 transition-all duration-300 ease-in-out
+          ${openMenu ? "-rotate-45 top-1/2 bg-black" : "top-5 bg-white"}
+        `}
+          />
+        </button>
+      </nav>
+      {openMenu && (
+        <div className="bg-white absolute top-0 h-screen w-full">
+          <ul className="flex flex-col h-screen justify-center items-center gap-2 text-2xl">
+            {navLinks.map((navLink, index) => (
+              <Link href={navLink.href} key={index}>
+                <li className="capitalize">{navLink.label}</li>
+              </Link>
+            ))}
+          </ul>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default MobileNavbar;
